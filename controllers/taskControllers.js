8dbd3e7a3dde0921016b6f5007a12a2c862fb004
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-16 20:32:38 +0530

    Created a Controller Function for a patch request on /todoList/tasks/ with Parameter id
    Added Update Request to Route /tasks/id & created a Controller Function which will take Id from /tasks/id & update the Status of the matching Document

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -16,12 +16,11 @@ const createTask = (req, res, next) => {
   newTask
     .save()
     .then((data) => {
-      console.log("Current Object", data);
+      res.send(data);
     })
     .catch((err) => {
       console.log(err);
     });
-  res.send(`New Task Created : ${req.body.taskName}`);
 };
 
 const deleteTask = (req, res, next) => {
@@ -34,8 +33,24 @@ const deleteTask = (req, res, next) => {
     });
 };
 
+const updateTask = (req, res, next) => {
+  TaskSchema.findByIdAndUpdate(
+    req.params.id,
+    { status: "Completed" },
+    { useFindAndModify: false, new: true }
+  )
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   getAllTasks,
   createTask,
   deleteTask,
+  updateTask,
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,11 +3,12 @@ const {
   getAllTasks,
   createTask,
   deleteTask,
+  updateTask,
 } = require("../controllers/taskControllers");
 const taskRouter = express.Router();
 
 taskRouter.route("/tasks").get(getAllTasks).post(createTask);
 
-taskRouter.route("/tasks/:id").delete(deleteTask);
+taskRouter.route("/tasks/:id").delete(deleteTask).patch(updateTask);
 
 module.exports = taskRouter;
