10bc02c28226a77a2b88aa1c310b3b1c06e85df9
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-16 12:35:45 +0530

    MongoDB Connection was added to the Express Server
    Created a MongoDB Connection to the Mongo Shell via MongoDB Localhost Connection String

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
 const app = express();
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, connection) => {
+    if (err) {
+      return console.log("Error in connecting to the Database.");
+    }
+    console.log("Connection to the Database was Successful.");
+  }
+);
 
 app.listen(
   process.env.PORT,
